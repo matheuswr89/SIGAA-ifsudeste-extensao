1e661d217a45a650ce42a2bf0432b9e78a43f699
matheuswr89 <matheuswr89@outlook.com>
2021-07-28 12:14:25 -0300

    primeiras alterações

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-//Tá um deserto aqui
+ALL[0].style.fontSize = "100%";
+ALL[0].style.background = "#FFF";
+if (CONTAINER != null) {
+  if (LOGIN != null) {
+    CONTAINER.style.height = "100vh";
+    CONTEUDO.style.marginTop = "30px";
+    RODAPE.style.position = "absolute";
+    RODAPE.style.width = "100%";
+  } else CONTAINER.style.height = "100%";
+  CONTAINER.style.width = "100vw";
+  CONTAINER.style.fontSize = "70%";
+}
+
+if (ALL[0].querySelector("#barraEsquerda") != null) {
+  ALL[0].style.fontSize = "0.7em";
+}
---
+++ selects.js
@@ -0,0 +1,6 @@
+const ALL = document.getElementsByTagName("body");
+const CONTAINER = document.querySelector("#container");
+const LOGIN = document.querySelector("#conteudo > .logon");
+const RODAPE = document.querySelector("#container > #rodape");
+const CONTEUDO = document.querySelector("#container > #conteudo");
+const INFO_USUARIO = document.querySelector("#painel-usuario > #info-usuario");
